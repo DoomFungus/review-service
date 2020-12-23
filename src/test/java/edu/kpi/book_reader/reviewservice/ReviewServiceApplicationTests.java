cf572b9930393b0fee0ac7aadaa798bdd3aa8865
DoomFungus <unspeakableferretofdoom@gmail.com>
2020-12-24 00:42:18 +0200

    Initial commit

---
+++ src/test/java/edu/kpi/book_reader/reviewservice/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kpi.book_reader.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
